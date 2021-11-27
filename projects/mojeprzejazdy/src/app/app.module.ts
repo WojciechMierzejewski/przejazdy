5849754c671d4836fe036313a5843c156e707edf
Slawomir Kussowski <skussowski@advaoptical.com>
2021-11-27 17:40:41 +0100

    introduced exemplary table to routes component

--- projects/mojeprzejazdy/src/app/app.module.ts
+++ projects/mojeprzejazdy/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,10 +12,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from "./components/login/login.component";
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { RoutesComponent } from './components/routes/routes.component';
+import { ScheduleDialogComponent } from './components/schedule/schedule-dialog/schedule-dialog.component';
 import { ScheduleComponent } from './components/schedule/schedule.component';
 import { UserregComponent } from './components/userreg/userreg.component';
-import { ScheduleDialogComponent } from './components/schedule/schedule-dialog/schedule-dialog.component';
-import { RoutesComponent } from './components/routes/routes.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +37,8 @@ import { RoutesComponent } from './components/routes/routes.component';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
