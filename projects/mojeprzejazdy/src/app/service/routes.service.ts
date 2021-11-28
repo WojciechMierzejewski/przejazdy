f544a42a80479f197e4dd1e2aca0c69d1a1e2817
Slawomir Kussowski <skussowski@advaoptical.com>
2021-11-28 12:35:32 +0100

    adjustments to use routes data service

--- projects/mojeprzejazdy/src/app/service/routes.service.ts
+++ projects/mojeprzejazdy/src/app/service/routes.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Route } from '../model/route';
 
+const data: Route[] = [
+  { id: 1, points: [], valid: true, schedules: [] },
+  { id: 2, points: [], valid: true, schedules: [] },
+  { id: 3, points: [], valid: true, schedules: [] },
+  { id: 4, points: [], valid: true, schedules: [] },
+  { id: 5, points: [], valid: true, schedules: [] },
+  { id: 6, points: [], valid: true, schedules: [] },
+  { id: 7, points: [], valid: true, schedules: [] },
+  { id: 8, points: [], valid: true, schedules: [] },
+  { id: 9, points: [], valid: true, schedules: [] },
+  { id: 10, points: [], valid: true, schedules: [] },
+];
 @Injectable({
   providedIn: 'root'
 })
 export class RoutesService {
 
   constructor() { }
+
+  fetchData(): Observable<Route[]> {
+    return of(data);
+  }
 }
