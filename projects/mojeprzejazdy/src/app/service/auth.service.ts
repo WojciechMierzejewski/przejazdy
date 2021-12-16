35e6017515373eccbf7828fe8ebe3892ab6e1c21
Slawomir Kussowski <skussowski@advaoptical.com>
2021-12-16 18:20:46 +0100

    login logic fixed

--- projects/mojeprzejazdy/src/app/service/auth.service.ts
+++ projects/mojeprzejazdy/src/app/service/auth.service.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(private http: HttpClient) {}
-  _loginUrl = 'https://awps-dev.herokuapp.com/user/login';
+  constructor(private http: HttpClient) { }
+  _loginUrl = '/api/user/login';
   authenticate(login: {
     login: string;
     password: string;
   }): Observable<boolean> {
-    if (this.http.post<any>(this._loginUrl, login)) {
-      return of(true);
-    } else {
-      return of(false);
-    }
+    return this.http.post<any>(this._loginUrl, login)
+      .pipe(catchError(e => of(false)),
+        map(value => {
+          console.log(value);
+          return value === false ? false : true;
+        }),
+      );
+
   }
 }
