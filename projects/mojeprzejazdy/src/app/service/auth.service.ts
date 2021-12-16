3a70f128d71da62419b81a4aeeaa0ae1182f5f73
mierzvoj <mierzvoj@gmail.com>
2021-12-16 21:52:15 +0100

    small local changes

--- projects/mojeprzejazdy/src/app/service/auth.service.ts
+++ projects/mojeprzejazdy/src/app/service/auth.service.ts
@@ -1,23 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(private http: HttpClient) { }
-  _loginUrl = '/api/user/login';
+  constructor(private http: HttpClient) {}
+  _loginUrl = '/api/user/user/login';
   authenticate(login: {
     login: string;
     password: string;
   }): Observable<boolean> {
-    return this.http.post<any>(this._loginUrl, login)
-      .pipe(catchError(e => of(false)),
-        map(value => {
-          console.log(value);
-          return value === false ? false : true;
-        }),
-      );
-
+    if (this.http.post<any>(this._loginUrl, login)) {
+      return of(true);
+    } else {
+      return of(false);
+    }
   }
 }
